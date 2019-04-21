dbc9254615f7257f0cbdbec391c4a2faf3a16aae
Kevin <kevin.bg@outlook.com>
2019-04-21 09:42:02 -0300

    Segundo Commit

--- src/test/java/cucumber/test/PrincipalTest.java
+++ src/test/java/cucumber/test/PrincipalTest.java
@@ -5,6 +5,8 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = {"pretty", "json:target/cucumber.json"} ,features = "src/test/resources", glue = "cucumber.test", monochrome = true)
+@CucumberOptions(plugin = {"pretty", "json:target/cucumber.json"},
+        features = "src/test/resources",
+        glue = "cucumber.test", monochrome = true)
 public class PrincipalTest {
 }
